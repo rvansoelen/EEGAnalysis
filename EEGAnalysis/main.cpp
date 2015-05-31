5cd4c3579bfca32a8314228a9fa7d53c8b80636a
rvansoelen <vansoelen.1@osu.edu>
2015-05-30 20:36:48 -0400

    Added Xcode stuff

---
+++ EEGAnalysis/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  EEGAnalysis
+//
+//  Created by Ryan Van Soelen on 5/30/15.
+//  Copyright (c) 2015 Mssn. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
